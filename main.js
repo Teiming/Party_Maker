fe0f06018a0eda8661871469974a1dbad4fcdb05
Teiming <36464297+Teiming@users.noreply.github.com>
2023-11-02 00:51:13 +0900

    ㅇ

--- main.js
+++ main.js
@@ -1,43 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
-    <title>스타레일 파티 생성기</title>
-</head>
-<body>
-    <hr>
-    <div id="characterBox">
-        <div class="character 바람 공허"><span>삼포</span></div>
-        <div class="character 얼음 공허"><span>페라</span></div>
-        <div class="character 양자 보존"><span>부현</span></div>
-        <div class="character 얼음 보존"><span>게파드</span></div>
-        <div class="character 얼음 보존"><span>Mar. 7th</span></div>
-        <div class="character 화염 보존"><span>불척자</span></div>
-        <div class="character 바람 수렵"><span>단항</span></div>
-        <div class="character 번개 지식"><span>서벌</span></div>
-        <div class="character 얼음 파멸"><span>경류</span></div>
-        <div class="character 허수 파멸"><span>음월</span></div>
-    </div>
-    <hr>
-    <h2>약점 선택</h2>
-    <div id="weak">
-        <div class="weakness 물리"><span>물리</span></div>
-        <div class="weakness 바람"><span>바람</span></div>
-        <div class="weakness 번개"><span>번개</span></div>
-        <div class="weakness 양자"><span>양자</span></div>
-        <div class="weakness 얼음"><span>얼음</span></div>
-        <div class="weakness 허수"><span>허수</span></div>
-        <div class="weakness 화염"><span>화염</span></div>
-    </div>
-    <hr>
-    <h3 id="job">
-        <span>딜러</span>
-        <span>버퍼</span>
-        <span>디버퍼</span>
-        <span>유지</span>
-    </h3>
-    <script src="main.js"></script>
-</body>
-</html>
+var character = {
+  물리: {
+    나타샤: "풍요",
+    루카: "공허",
+    소상: "수렵",
+  },
+  바람: {},
+  번개: {},
+  양자: {},
+  얼음: {},
+  허수: {
+    음월: "파멸",
+    정운: "화합",
+  },
+  화염: {
+    아스타: "화합",
+    후크: "파멸",
+    계네빈: "공허",
+  },
+};
+for (let i = 0; i < document.getElementsByClassName("weakness").length; i++) {
+  document.getElementsByClassName("weakness")[i].style.opacity = "20%";
+}
+
+function selweak(x) {
+  var targetweak = document.getElementById(x);
+  console.log(targetweak.style.opacity);
+  if (targetweak.style.opacity) {
+    targetweak.style.opacity = null;
+    jobasg(x);
+  } else {
+    targetweak.style.opacity = "50%";
+    jobdel(x);
+  }
+}
+function jobasg(x) {
+  for (let i = 0; i < Object.keys(character[x]).length; i++) {
+    document.getElementById("log").innerText = Object.keys(character["물리"]);
+    const element = Object.keys(character[x])[i];
+  }
+}
+function jobdel(x) {}
+document.getElementById("log").innerText = Object.keys(character["물리"]);
